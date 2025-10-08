7705771ecf9db6bb297090b3a31a324a7b308968
vow130504 <22120344@student.hcmus.edu.vn>
2025-10-08 13:03:55 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "IA01-React-tutorial"
+  base: '/IA01-React-tutorial'
 })
